54992dede0001ef7d5015bfaecff9e271f0014ba
Maria A. Nieves Colon <mnievesc@asu.edu>
2017-09-02 13:46:27 -0700

    Update 2_downsample.sh

--- 2_downsample.sh
+++ 2_downsample.sh
@@ -4,10 +4,12 @@
 ## Written by M. Nieves-Colon - 2/12/2017
 ## To downsample I chose the lowest number of merged reads within an individual pair.
 
+# Usage on cluster: sbatch 2_downsample.sh
+
 # Following instructions from GitHUB README page for seqtk:
-> Subsample 10000 read pairs from two large paired FASTQ files (remember to use the same random seed to keep pairing):
-`seqtk sample -s100 read1.fq 10000 > sub1.fq`
-`seqtk sample -s100 read2.fq 10000 > sub2.fq`
+# > Subsample 10000 read pairs from two large paired FASTQ files (remember to use the same random seed to keep pairing):
+# `seqtk sample -s100 read1.fq 10000 > sub1.fq`
+# `seqtk sample -s100 read2.fq 10000 > sub2.fq`
 
 # I used the following commands on Saguaro cluster to subsample smallest number in a pair after SeqPrep merging and adapter trimming:
 
